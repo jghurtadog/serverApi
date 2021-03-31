30a39b396cb54443bb7a576dbc61a40fa89d409e
Jose Hurtado <57419008+jhurtadog@users.noreply.github.com>
2021-03-30 22:45:58 -0500

    ajuste

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ app.use(cors());
 
 app.use(express.json({ extended: true }));
 
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/projects", require("./routes/projects"));
 app.use("/api/tasks", require("./routes/tasks"));
 
-app.listen(PORT, () => {
-  console.log(`El servidor esta funcionando en el puesto ${PORT}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`El servidor esta funcionando en el puesto ${port}`);
 });
